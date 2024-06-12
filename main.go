e94f042f28cfca49a3f9c52d858dc32684c7cfa5
Artem Veber <veberav1201@yandex.ru>
2024-06-12 12:49:14 +0700

    Добавлены файлы проекта

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func encode(s string) string {
+	n := "Tinkoff"
+	tcount := 0
+	icount := 0
+	ncount := 0
+	kcount := 0
+	ocount := 0
+	fcount := 0
+	for _, v := range n {
+		for _, i := range s {
+			if i == v {
+				switch string(i) {
+				case "T":
+					tcount++
+				case "i":
+					icount++
+				case "n":
+					ncount++
+				case "k":
+					kcount++
+				case "o":
+					ocount++
+				case "f":
+					fcount++
+
+				}
+			}
+		}
+	}
+	minMatch := min(tcount, icount, ncount, kcount, ocount, fcount/2)
+	result, _ := fmt.Printf("Слово Tinkoff встречается %d", minMatch)
+	return strconv.Itoa(result)
+}
+
+func main() {
+
+	encode("nkoffTiTinkoff")
+}
